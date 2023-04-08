3aff7243ef6439061914d412e8002a128e9623a2
Ali Said Sarıtemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2023-04-09 00:05:42 +0300

    Find A Sub-Word's Solving Added

---
+++ FindASub-Word.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+public class Solution {
+
+    public static void main(String[] args) {
+  
+         Scanner scanner = new Scanner(System.in);
+                
+          String countOfSentencesString = scanner.nextLine();
+          int countOfSentences=Integer.parseInt(countOfSentencesString);
+         List<String> sentencesList=new ArrayList<>();
+         List<String> queriesList=new ArrayList<>();
+         
+         for(int i =0;i<countOfSentences;i++){
+             sentencesList.add(scanner.nextLine());            
+         }
+         String quarieNumString= scanner.nextLine();
+         int quarieNum=Integer.parseInt(quarieNumString);
+         
+          for(int i =0;i<quarieNum;i++){
+             queriesList.add(scanner.nextLine());            
+         }
+         String queriesForPattern="";
+    
+         for (String query : queriesList) {
+        
+           
+          Pattern p = Pattern.compile("\\w("+query+")\\w"); 
+          int resultCounter=0;
+          
+          for (String string : sentencesList) {
+               Matcher m = p.matcher(string);
+              
+                 while(m.find())    
+                     resultCounter++;
+            
+          }
+
+          System.out.println(resultCounter);
+           }  
+     
+    }
+}

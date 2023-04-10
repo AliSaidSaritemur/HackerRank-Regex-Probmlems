863288c8f38995690a22ca8dfce6bf273ecca688
Ali Said Sarıtemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2023-04-10 17:59:29 +0300

    Valid PAN format's Solving Added

---
+++ ValidPANFormat.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     
+         Pattern p = Pattern.compile("^[A-Z]{5}[0-9]{4}[A-Z]$");
+         Scanner scanner = new Scanner(System.in);
+           String temp = scanner.nextLine();
+         while(scanner.hasNext()){       
+          String line = scanner.nextLine();
+          Matcher m = p.matcher(line);
+          if(m.find())
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+         }
+    }
+}
